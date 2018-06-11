22f0d79adacbd3bf34afdccabb2e4fdac1f32554
Denis Orehovsky <denis.orehovsky@gmail.com>
2018-06-11 09:52:06 +0300

    Create jokes index page

--- thisisproject/urls.py
+++ thisisproject/urls.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls)]
+urlpatterns = [path("admin/", admin.site.urls), path("jokes/", include("jokes.urls"))]
